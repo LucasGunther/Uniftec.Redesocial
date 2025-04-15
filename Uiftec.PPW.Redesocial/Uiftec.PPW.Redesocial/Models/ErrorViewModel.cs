207c197c4355df619382c573816fc1ddf228a024
LucasGunther <Lucas.gunther00@gmail.com>
2025-04-15 20:43:28 -0300

    commit inicial

---
+++ Uiftec.PPW.Redesocial/Uiftec.PPW.Redesocial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Uiftec.PPW.Redesocial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
