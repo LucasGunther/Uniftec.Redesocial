cc49a2cc1bdf48d3b8c49b44326f993050a54b0d
PatiahcoR <112357591+PatiahcoR@users.noreply.github.com>
2025-08-13 01:01:14 -0300

    Delete Uiftec.PPW.Redesocial directory

--- Uiftec.PPW.Redesocial/Uiftec.PPW.Redesocial/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Uiftec.PPW.Redesocial.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
